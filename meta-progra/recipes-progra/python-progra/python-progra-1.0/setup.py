998484059fe1200c934102b78feb0d3ebd30703b
pauclari <66702741+pauclari@users.noreply.github.com>
2020-08-17 08:33:48 -0600

    Create setup.py

---
+++ meta-progra/recipes-progra/python-progra/python-progra-1.0/setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "progra",
+    version = "0.1",
+    packages = ["progra"],
+    author = "Paula",
+    author_email = "pau@example.com",
+    description  = "Programa para el reconocimiento facial",
+    license = "MIT",
+    keywords = "example",
+    url = "",
+)
